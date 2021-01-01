9fbd631daf978d56e1101cc49385dc595d1406f6
shyshallot2006 <commanderpandayt@gmail.com>
2021-01-01 11:08:20 -0800

    Added Paris, Fixed Gladius Turrets, Improvements

--- Scripts/GameObject/BoardingAbility.lua
+++ Scripts/GameObject/BoardingAbility.lua
@@ -4,6 +4,9 @@ require("HALOFunctions")
 -- Script is used for Humanity's Retaliation 
 -- Boarding Script is written by ShyShallot, If you wish to use this script please contact the Project Gold Team via Discord
 -- Any use of this script without permission will not be fun for offending party.
+-- Any Modifications of this Script for Submods of Humanity's Retliation are allowed as long as they stay in this mod 
+-- Even if you modify this script for a submod, you still do not have permission to use else where
+
 -- Lua Doc: https://stargate-eaw.de/media/kunena/attachments/92/LuacommandsinFoC.pdf
 
 -- Even tho Tractor Beams and Boarding are 2 unrelated abilities, the Tractor Beam allows us to target a specfic ship and check 
@@ -25,13 +28,12 @@ function State_Init(message)
     if message == OnEnter then
         DebugMessage("%s -- In Init", tostring(Script))
         ability_name = "TRACTOR_BEAM" -- Check if the object calling the script has the ability in the first place
-        UntilBoardChances = 0  
-        TotalBoardUses = 0
-        ShouldRun = 1
-        AIRUN = 0
-        InitalBoardingChance = 0.45
-        TakeOverChance =  0.95
-        FailChance = 0.6
+        UntilBoardChances = 0  -- Used to check if we should start using chances during the building, like to cancel or take over the ship
+        TotalBoardUses = 0 -- Used to destory the Tractor Beam Hardpoint after X amount of uses
+        ShouldRun = 0 -- Should we loop the Damage and Chance functions during boarding
+        InitalBoardingChance = 0.45 -- The Intial Chance for the boarding to begin
+        TakeOverChance =  0.95 -- Used for the chance of taking over the ship
+        FailChance = 0.6 -- The Chance for the boarding to fail and stop
         player = Object.Get_Owner() -- Since we cant Use PlayerObject directly, get the player from the Object calling this script
         
    
@@ -41,7 +43,7 @@ function State_Init(message)
             if Object.Is_Ability_Active(ability_name) then -- If Tractor Beam ability is active
                 DebugMessage("%s -- Tractor Beam is now active running function", tostring(Script))
                 Find_Nearest_Board_Target(false)
-            else Sleep(2) end
+            end
         else 
             DebugMessage("%s -- Running AI Ability", tostring(Script))
             Set_Next_State("State_AI_Autofire")
@@ -52,12 +54,12 @@ end
 function State_AI_Autofire(message)
     if message == OnUpdate then
         DebugMessage("%s -- Is AI Checking for Ability", tostring(Script))
-        if Object.Is_Ability_Ready(ability_name) then
+        if Object.Is_Ability_Ready(ability_name) then -- Is the Boarding Ability ready to use
             DebugMessage("%s -- Running Ability from AI", tostring(Script))
             Object.Activate_Ability(ability_name, true)
             if Object.Is_Ability_Active(ability_name) then -- If Tractor Beam ability is active
                 DebugMessage("%s -- Tractor Beam is now active running function from AI", tostring(Script))
-                Find_Nearest_Board_Target(true)
+                Find_Nearest_Board_Target(true) -- Run this function with true to let the Function know the AI is using it
             else Sleep(2) end
 		end
 	end		
@@ -77,7 +79,7 @@ function Find_Nearest_Board_Target(AIRUN)
             ShouldRun = 1
             BoardingFunction()
         end
-    else Sleep(2) end  
+    else Sleep(1) end  -- Cant find Target sleeping for 1 seconds to give time to find one.
 end
 
 
@@ -109,7 +111,7 @@ function BoardingFunction()
                             if Return_Chance(FailChance)  then -- If the boarding units die by chance
                                 Sleep(3)
                                 ShouldRun = 0
-                                target = nil
+                                target = nil -- Set our target as Null or Nil so the script stops damaging the ship
                                 boardingActive = false -- Boarding No Longer active, exit loop
                                 Object.Cancel_Ability(ability_name) -- Make sure the "Tractor Beam" ability stops
                                 Object.Play_SFX_Event("SFX_UM02_MagneticSealedDoor")
@@ -148,6 +150,6 @@ function BoardingFunction()
                 end
             end
         end
-    else ShouldRun = 0 end
+    else ShouldRun = 0 end -- If the target we found isnt Alive and/or Not being affected by the Tractor Beam essentially restart the script
 end
 
