6f6f827d1abfc1fcac7ab621a76d2f0f6f93fcf7
ShyShallot <commanderpandayt@gmail.com>
2023-10-11 09:45:15 -0700

    some balance changes and fixes, added sdv as buildable

--- Scripts/GameObject/ObjectScript_SelfDestructStation.lua
+++ Scripts/GameObject/ObjectScript_SelfDestructStation.lua
@@ -15,6 +15,8 @@ function State_Init(message)
         player = Object.Get_Owner()
         station = player.Get_Space_Station()
         if TestValid(station) then
+            Game_Message("The " .. player.Get_Faction_Name() .. " player is a pussy, initiating self destruct in 5 seconds")
+            Sleep(5)
             station.Take_Damage(100000000000000)
         end
 	end
